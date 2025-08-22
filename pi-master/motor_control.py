74779218964f35e5ea435e39e38c7f4e3434a6f0
der <dave@epimorphics.com>
2025-08-22 15:53:17 +0100

    Rationalize BLE connection

--- pi-master/motor_control.py
+++ pi-master/motor_control.py
@@ -40,30 +40,34 @@ class MotorController:
     async def handle_disconnect(self, _: BleakClient):
         print("Device disconnected, retrying")
         self.is_connected = False
-        await self.connect()
 
     async def connect(self):
-        print(f'Connecting to {self.device.name}')
-        async with BleakClient(self.device, disconnected_callback=self.handle_disconnect) as client:
-            print('Connected to rover')
-            self.is_connected = True
-            rover = client.services.get_service(UART_SERVICE_UUID)
-            rx = rover.get_characteristic(UART_RX_CHAR_UUID)
-
-            while True:
-                # Check if there's a command in the queue without blocking
+        while True:
+            print(f'Connecting to {self.device.name}')
+            async with BleakClient(self.device, disconnected_callback=self.handle_disconnect) as client:
                 try:
-                    command = self.queue.pop()
-                    if command == 'x':
-                        print('Quit requested')
-                        self.is_connected = False
-                        await client.disconnect()
-                        return
-                    print(f"Sending {command}")
-                    await client.write_gatt_char(rx, command.encode(), response=False)
-                except IndexError:
-                    # Give control back to the event loop to allow other tasks to run
-                    await asyncio.sleep(0.01)
+                    client.connect()
+                    print('Connected to rover')
+                    self.is_connected = True
+                    rover = client.services.get_service(UART_SERVICE_UUID)
+                    rx = rover.get_characteristic(UART_RX_CHAR_UUID)
+
+                    while self.is_connected:
+                        # Check if there's a command in the queue without blocking
+                        try:
+                            command = self.queue.pop()
+                            if command == 'x':
+                                print('Quit requested')
+                                self.is_connected = False
+                                await client.disconnect()
+                                return
+                            print(f"Sending {command}")
+                            await client.write_gatt_char(rx, command.encode(), response=False)
+                        except IndexError:
+                            # Give control back to the event loop to allow other tasks to run
+                            await asyncio.sleep(0.01)
+                except Exception as e:
+                    print(e)
 
     def send(self, speed: int, dir: str):
         self.queue.append(f"{speed}{dir}")
